8394f26379e6d63b58ec8fa8da0b7862bc716915
webkawsar <web.kawsarahmed@gmail.com>
2022-12-22 23:26:58 +0600

    practice 2nd way but it not correct

--- week_2.2/Two Sum.js
+++ week_2.2/Two Sum.js
@@ -1,28 +1,57 @@
 
 
+
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 // O(n^2)
 function twoSum(nums, target) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
 
+console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSum([3,2,4], 6))
+
+
+
+
+/****************Way 2 *************/
+
+// O()
+function twoSum2(nums, target) {
+
+    const hashTable = {};
     for (let i = 0; i < nums.length; i++) {
+        hashTable[nums[i]] = i
+    }
 
-        const element = nums[i];
-        for (let j = 1; j < nums.length; j++) {
+    console.log(hashTable)
+    
+    for (const value in hashTable) {
 
-            const element2 = nums[j];
-            if(element + element2 === target){
-                return [i, j]
-            }
+        const firstIndex = hashTable[value];
+        const remained = target > value ? target - value : value - target;
 
+        console.log(firstIndex)
+        console.log(remained);
+
+        if(remained in hashTable) {
+            const secondIndex = hashTable[remained];
+            return [firstIndex, secondIndex]
         }
     }
 }
 
-console.log(twoSum([2,7,11,15], 9))
-console.log(twoSum([3,2,4], 6))
+// console.log(twoSum2([5, 25, 20, 8], 13));
+// console.log(twoSum2([3,2,4], 6))
+console.log(twoSum2([3, 3], 6))
 
 
 
