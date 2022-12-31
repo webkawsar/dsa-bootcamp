ae62c15540a0c6cb70c3722efd9d47417f00d83b
webkawsar <web.kawsarahmed@gmail.com>
2022-12-31 11:03:20 +0600

    practice angram myself

---
+++ week_2.2_Frequency_Counter_Pattern/242. Valid Anagram.js
@@ -0,0 +1,46 @@
+
+
+
+
+function isAnagram(s, t) {
+
+    if(s.length !== t.length) return false;
+
+    const hashTableA = {};
+    const hashTableB = {};
+
+    for (let i = 0; i < s.length; i++) {
+
+        const element = s[i];
+        hashTableA[element] = (hashTableA[element] || 0) + 1
+        
+    }
+
+    for (let i = 0; i < t.length; i++) {
+
+        const element = t[i];
+        hashTableB[element] = (hashTableB[element] || 0) + 1
+        
+    }
+
+    for (const key in hashTableA) {
+        
+        if(hashTableA[key] === hashTableB[key]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// console.log(isAnagram('anagram', 'nagaram'));
+// console.log(isAnagram('rat', 'car'));
+// console.log(isAnagram('a', 'ab'));
+console.log(isAnagram('anagtam', 'nbgbram'));
+
+
+
+
+
