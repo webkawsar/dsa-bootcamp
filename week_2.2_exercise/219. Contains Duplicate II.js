f0e28b60e62d2208fd045c8fe364f2b2bd673015
webkawsar <web.kawsarahmed@gmail.com>
2023-01-08 11:51:21 +0600

    solve 219 no. exercise problem

--- week_2.2_exercise/219. Contains Duplicate II.js
+++ week_2.2_exercise/219. Contains Duplicate II.js
@@ -1,24 +1,69 @@
 // Difficulty level => Easy
-
 // https://leetcode.com/problems/contains-duplicate-ii/
 
 
-
-
+// time complexity => O(n^2)
+// nums[i] == nums[j] and abs(i - j) <= k.
 function containsNearbyDuplicate(nums, k) {
-
-
-
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if(nums[i] == nums[j] && Math.abs(i-j) <= k) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-console.log(containsNearbyDuplicate([1,2,3,1], 3)); // true
-console.log(containsNearbyDuplicate([1,0,1,1], 1)); // true
-console.log(containsNearbyDuplicate([1,2,3,1,2,3], 2)); // false
+// console.log(containsNearbyDuplicate([1,2,3,1], 3)); // true
+// console.log(containsNearbyDuplicate([1,0,1,1], 1)); // true
+// console.log(containsNearbyDuplicate([1,2,3,1,2,3], 2)); // false
 
 
 
 
 
+
+// Alternative way
+
+// nums[i] == nums[j] and abs(i - j) <= k
+function containsNearbyDuplicate2(nums, k) {
+
+    const hashTable = {};
+    for (let i = 0; i < nums.length; i++) {
+
+        const element = nums[i];
+        const hashTableElm = hashTable[element];
+        if(nums[hashTableElm] === element && Math.abs(i - hashTable[element]) <= k){
+            return true;
+        } else {
+            hashTable[element] = i;
+        }
+
+    }
+
+    return false;
+}
+
+
+
+// console.log(containsNearbyDuplicate2([1,2,3,1], 3)); // true
+// console.log(containsNearbyDuplicate2([1,0,1,1], 1)); // true
+console.log(containsNearbyDuplicate2([1,2,3,1,2,3], 2)); // false
+
+
+
+
+//      let hashTable = [];
+//     for(let i = 0; i < nums.length; i++) {
+//     // first case will be undefined in hashTable & hence NAN
+//         if(i - hashTable[nums[i]] <= k) {
+//             return true;
+//         } else {
+//             hashTable[nums[i]] = [i] ;
+//         }
+//     }
+//     return false;
 
 
 
--- week_2.2_exercise/exercise.js
+++ week_2.2_exercise/exercise.js
@@ -11,21 +11,21 @@
 // sameFrequency(3589578, 5879385) // true
 // sameFrequency(22,222) // false
  
-219. Contains Duplicate II
-https://leetcode.com/problems/contains-duplicate-ii/
-Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
-Example 1:
+// 219. Contains Duplicate II
+// https://leetcode.com/problems/contains-duplicate-ii/
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+// Example 1:
  
-Input: nums = [1,2,3,1], k = 3
-Output: true
-Example 2:
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
  
-Input: nums = [1,0,1,1], k = 1
-Output: true
-Example 3:
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
  
-Input: nums = [1,2,3,1,2,3], k = 2
-Output: false
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
  
 // 442. Find All Duplicates in an Array
 // https://leetcode.com/problems/find-all-duplicates-in-an-array/
