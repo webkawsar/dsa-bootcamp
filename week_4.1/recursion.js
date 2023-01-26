a6831a1466b256e3c98cdc529f3c93f212ad3e45
webkawsar <web.kawsarahmed@gmail.com>
2023-01-26 20:56:41 +0600

    practice recursion pure function way

--- week_4.1/recursion.js
+++ week_4.1/recursion.js
@@ -58,3 +58,46 @@ function sumRange2(num) {
 }
 
 console.log(sumRange2(10))
+
+
+
+
+// iterative solution
+function createRange(num1, num2) {
+    let rangeArr = [];
+    for (let i = num1; i <= num2; i++) {
+        rangeArr.push(i);
+    }
+    console.log(rangeArr)
+}
+createRange(10, 20)
+
+
+
+// impure recursive solution
+function createRange2(num1, num2) {
+    let rangeArr = [];
+    function helper(inputNum, targetNum) {
+        rangeArr.push(inputNum);
+
+        // base case
+        if(inputNum === targetNum) return targetNum;
+        helper(inputNum + 1, targetNum)
+    }
+    helper(num1, num2);
+    console.log(rangeArr)
+}
+createRange2(10, 20)
+
+
+
+
+// pure recursive solution
+function createRange3(num, targetNum, rangeArr = []) {
+    rangeArr.push(num);
+    // base case
+    if(num === targetNum) return rangeArr;
+    return createRange3(num + 1, targetNum, rangeArr);
+}
+
+console.log(createRange3(10, 20);
