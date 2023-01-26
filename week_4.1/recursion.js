1c8813934002c520a3142c596a9065f1e0ed31cf
webkawsar <web.kawsarahmed@gmail.com>
2023-01-26 19:22:48 +0600

    practce range in recursion

--- week_4.1/recursion.js
+++ week_4.1/recursion.js
@@ -19,6 +19,8 @@ countToZero(10);
 function countToZero2(num) {
 
     console.log(num)
+
+    // base case
     if(num === 0) return
 
     countToZero2(num - 1)
@@ -27,3 +29,32 @@ function countToZero2(num) {
 countToZero2(10);
 
 
+
+
+
+// iterative solution
+function sumRange(num1, num2) {
+
+    let total = 0;
+    for (let i = 0; i <= num2; i++) {
+        total += i;
+    }
+
+    console.log(total)
+}
+
+sumRange(1, 10);
+
+
+
+
+
+// recursive solution
+function sumRange2(num) {
+
+    // base case
+    if(num === 0) return 0;
+    return num + sumRange2(num - 1);
+}
+
+console.log(sumRange2(10))
