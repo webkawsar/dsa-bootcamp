cb2619465e174f61cc18b1693583dcfdd351ab5c
webkawsar <web.kawsarahmed@gmail.com>
2023-02-02 21:38:05 +0600

    practice bubble sort

---
+++ week_5.1/bubble_sort.js
@@ -0,0 +1,30 @@
+
+
+
+// Bubble Sort
+// Move the largest element to he end
+
+// Time Complexity => O(n^2)
+// Space Complexity => o(n)
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let swap = false;
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            console.log(arr, arr[j], arr[j+1])
+            if(arr[j] > arr[j + 1]) {
+                let temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                swap = true;
+                
+            }
+        }
+
+        if(!swap) break;
+    }
+
+    return arr;
+}
+
+// console.log(bubbleSort([10, 1, -2, 2, 5, 3]));
+console.log(bubbleSort([1, 10, 2, 20, 6, 30, 9]));
