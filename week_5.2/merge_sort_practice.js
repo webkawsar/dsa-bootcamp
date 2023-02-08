5168744b174f9073c96ab11ce1f754a9232f41d8
webkawsar <web.kawsarahmed@gmail.com>
2023-02-08 20:34:32 +0600

    practice merge sort

--- week_5.2/merge_sort_practice.js
+++ week_5.2/merge_sort_practice.js
@@ -28,7 +28,6 @@ function merge(arr1, arr2) {
     return results;
 }
 
-
 function mergeSort(arr) {
 
     let mid = Math.floor(arr.length / 2);
@@ -39,3 +38,42 @@ function mergeSort(arr) {
 }
 
 console.log(mergeSort([1, 3, 5, 9, 7, 11]));
+
+
+
+function merge2(arr1, arr2) {
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if(arr1[i] < arr2[j]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results;
+}
+
+function mergeSort2(arr) {
+    let mid = Math.floor(arr.length / 2);
+    if(arr.length <= 1) return arr;
+    let left = mergeSort2(arr.slice(0, mid));
+    let right = mergeSort2(arr.slice(mid));
+    return merge2(left, right);
+}
+
+console.log(mergeSort2([1, 3, 5, 9, 7, 11]));
