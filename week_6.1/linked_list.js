3031495d72793d2cffcd96295e3450842e1ccae6
webkawsar <web.kawsarahmed@gmail.com>
2023-02-11 12:41:04 +0600

    practice linked list introduction

---
+++ week_6.1/linked_list.js
@@ -0,0 +1,33 @@
+
+
+// Linked List
+// Singly Linked List
+// Doubly Linked List
+
+// Linked  list e node thake
+
+
+class Person {
+    // properties
+    leg = 2
+    hand = 2
+
+    constructor(leg, hand) {
+        this.leg = leg;
+        this.hand = hand;
+    }
+
+
+    // method
+    walk() {
+        return 'Kawsar na hete shudu duray'
+    }
+}
+
+const kawsar = new Person(3, 3);
+console.log(kawsar)
+console.log(kawsar.walk())
+
+
+
+
