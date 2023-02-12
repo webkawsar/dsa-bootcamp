70a7359419d72a45d3f6ff10cbc19c893c23594b
webkawsar <web.kawsarahmed@gmail.com>
2023-02-12 11:12:47 +0600

    removing item at the end

--- week_6.1/linked_list.js
+++ week_6.1/linked_list.js
@@ -1,32 +1,111 @@
 
 
-// Linked List
-// Singly Linked List
-// Doubly Linked List
-
-// Linked  list e node thake
 
 
-class Person {
-    // properties
-    leg = 2
-    hand = 2
 
-    constructor(leg, hand) {
-        this.leg = leg;
-        this.hand = hand;
-    }
-
-
-    // method
-    walk() {
-        return 'Kawsar na hete shudu duray'
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
 }
 
-const kawsar = new Person(3, 3);
-console.log(kawsar)
-console.log(kawsar.walk())
+// const node = new Node(10);
+// console.log(node)
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1
+    }
+
+    // adding node at the end of the linked list
+    // time complexity => O(1)
+    push(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    // adding node at the beginning
+    // time complexity => O(1)
+    unshift(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            // const temp = this.head;
+            // this.head = newNode;
+            // this.head.next = temp;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+    }
+
+    // remove from the beginning
+    // time complexity O(1)
+    shift() {
+        if(!this.head) return null;
+
+        const temp = this.head;
+        this.head = this.head.next;
+        this.length--;
+        temp.next = null;
+
+        if(this.length === 0) {
+            this.tail = null;
+        }
+        return temp;
+    }
+
+    // removing item at the end
+    // time complexity => O(n)
+    /*
+
+                   pre
+        10--> 11--> 12--> 13--> null
+                         temp
+
+    */
+    pop() {
+        if(!this.head) return null;
+
+        let temp = this.head;
+        let pre = this.head;
+        while (temp.next) {
+            pre = temp;
+            temp = temp.next
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return temp;
+    }
+}
+
+const linkedList = new LinkedList(10);
+console.log(linkedList.push(11))
+console.log(linkedList.unshift(9))
+console.log(linkedList.shift());
+console.log(linkedList.pop());
+
+console.log(linkedList)
 
 
 
