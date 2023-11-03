32758ed1e8c19b9baf9ba8e4b2012488bf9d8ab8
Kawsar <web.kawsarahmed@gmail.com>
2023-11-03 14:36:20 +0600

    minor changes

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 
 
+var filter = function(arr, fn) {
+    
+    let filteredArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        
+        const element = arr[i];
+        if(fn(element, i)) {
+            filteredArr.push(element)
+        }
+    }
+
+    return filteredArr;
+};
 
 
+let arr = [0, 10, 20, 30];
+function greaterThan10(n) { 
+    if(n > 10) return n;
+}
 
-
-
-
-
-
-
-
-
+console.log(filter(arr, greaterThan10))
